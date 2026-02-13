6bbeed49ff51d7194113ab53124c2e22fe809bca
Bisrat Beriso <bisratberiso@gmail.com>
2026-02-13 16:59:14 +0300

    Time: 6 ms (17.29%) | Memory: 11 MB (8.51%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+class Solution {
+    public:
+        bool isPalindrome(int x) {
+            string numStr=std::to_string(x);
+            int i=0;
+            int j=numStr.length()-1;
+            while(i<=j){
+                if (numStr[i]!=numStr[j]){
+                return false;}
+                    i++;
+                    j--;}
+            return true;
+            }
+            
+    };
