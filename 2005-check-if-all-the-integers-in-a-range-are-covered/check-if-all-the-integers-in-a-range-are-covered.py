c76915dc81428e553e118efa7fec9fbf04d119de
Bisrat Beriso <bisratberiso@gmail.com>
2026-02-20 17:46:57 +0300

    Time: 0 ms (100.00%) | Memory: 19.4 MB (23.05%) - LeetSync

--- 2005-check-if-all-the-integers-in-a-range-are-covered/check-if-all-the-integers-in-a-range-are-covered.py
+++ 2005-check-if-all-the-integers-in-a-range-are-covered/check-if-all-the-integers-in-a-range-are-covered.py
@@ -5,6 +5,7 @@ class Solution:
             for j in ranges:
                 if j[0]<=i<=j[1]:
                     check=True
+                    break
             if not check:
                 return False
         return True
