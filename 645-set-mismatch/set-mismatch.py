3e4e26ceb5aad6727cc7aa97f0440de57cf12ead
Bisrat Beriso <bisratberiso@gmail.com>
2026-04-18 16:55:35 +0300

    Time: 11 ms (67.75%) | Memory: 20.6 MB (57.06%) - LeetSync

---
+++ 645-set-mismatch/set-mismatch.py
@@ -0,0 +1,19 @@
+class Solution:
+    def findErrorNums(self, nums):
+        n = len(nums)
+        freq = [0] * (n + 1)
+        
+        # Count frequency
+        for num in nums:
+            freq[num] += 1
+        
+        duplicate = -1
+        missing = -1
+        
+        for i in range(1, n + 1):
+            if freq[i] == 2:
+                duplicate = i
+            elif freq[i] == 0:
+                missing = i
+        
+        return [duplicate, missing]
